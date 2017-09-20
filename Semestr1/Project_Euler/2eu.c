0a69e6d82006b1bc0df9b116e67c8f8b80199eb5
Shonchik <31989755+Shonchik@users.noreply.github.com>
2017-09-20 12:54:42 +0700

    Problem 2

---
+++ Semestr1/Project_Euler/2eu.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	int fib_1 = 1;
+	int fib_2 = 2;
+	int fib_3 = 0;
+	int sum = 0;
+
+	while (fib_2 < 4000000) {
+		if (fib_2 % 2 == 0) {
+			sum += fib_2;
+		}
+		fib_3 = fib_2 + fib_1;
+		fib_1 = fib_2;
+		fib_2 = fib_3;
+	}
+
+	printf("%d", sum);
+
+}
