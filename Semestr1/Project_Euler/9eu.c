c3c81c5923e3bf82fc71fa3fe6b5d41c5566e50a
Shonchik <31989755+Shonchik@users.noreply.github.com>
2017-10-07 12:14:21 +0700

    Problem 9

--- Semestr1/Project_Euler/9eu.c
+++ Semestr1/Project_Euler/9eu.c
@@ -1,24 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+
+#define summ 1000
 
 int main()
 {
-	
 	int a = 0;
 	int b = 0;
 	int c = 0;
 	int proverka = 0;
 
 	for (int i = 499; i > 1; i--) {
-		for (int j = 1000 - i; j > 1; j--) {
-			if (i * i == j * j + (1000 - i - j) * (1000 - i - j)) {
+		for (int j = summ - i; j > 1; j--) {
+			if (i * i == j * j + (summ - i - j) * (summ - i - j)) {
 				proverka = 1;
 				c = i;
 				b = j;
-				a = 1000 - i - j;
+				a = summ - i - j;
 				break;
 			}
 		}
-		if (proverka == 1) {
+		if (proverka) {
 			break;
 		}
 	}
