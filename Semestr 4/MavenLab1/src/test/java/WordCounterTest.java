7bb748374ca5f3dcb35c4d3f9e79b3d8248dac69
Shonchik <31989755+Shonchik@users.noreply.github.com>
2019-06-26 22:46:49 +0700

    4

--- Semestr 4/MavenLab1/src/test/java/WordCounterTest.java
+++ Semestr 4/MavenLab1/src/test/java/WordCounterTest.java
@@ -1,10 +1,13 @@
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class WordCounterTest {
 
@@ -30,9 +33,9 @@ class WordCounterTest {
         }
 
         try (BufferedReader fileReader = new BufferedReader(new FileReader("2.csv"))) {
-            String str = fileReader.readLine();
-
-            assertTrue(str.equals("odin;1;1.0%"));
+            //String str = fileReader.readLine();
+            assertTrue(FileUtils.contentEquals(new File("2.csv"), new File("test.csv")));
+            //FileUtils.contentEquals("2.csv", "test.csv");
 
         } catch (Exception e) {
             System.err.println("ERROR: " + e.getMessage());
