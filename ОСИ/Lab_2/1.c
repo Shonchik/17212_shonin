c66d8a8a7f04fea540f1145be9ddc761d18960ed
Shonchik <31989755+Shonchik@users.noreply.github.com>
2020-10-20 12:21:21 +0700

    new Lab_2

--- ОСИ/Lab_2/1.c
+++ ОСИ/Lab_2/1.c
@@ -3,42 +3,46 @@
 #include <pthread.h>
 #include <string.h>
 
-#define NOTERROR 0
+#define THREAD_CREATION_SUCCESS 0
+#define THREAD_JOIN_SUCCESS 0
+#define PRINT_COUNT 10
+#define BUFFER_SIZE 256
+#define EXIT_ERROR 1
 
-void * thread_body(void * param) {
-     
-    int i = 0;
-     for (i = 0; i < 10; i++) {
-        printf("Child1\n");
-     }
+void printLines(const char *str) {
+    int i;
+    for(i = 0; i < PRINT_COUNT; i++)
+        printf(str);
+}
 
+void printError(int code, char **argv) {
+    char buf[BUFFER_SIZE];
+    strerror_r(code, buf, sizeof buf);
+    printf("%s: %s\n", argv[0], buf);
+}
+
+void* thread_body(void * param) {
+    printLines(param);
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t thread;
     int code;
-    int code2;
-    
-    code=pthread_create(&thread, NULL, thread_body, NULL);
-    if (code!=NOTERROR) {
-        char buf[256];
-        strerror_r(code, buf, sizeof buf);
-        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
-        exit(1);
+
+    code = pthread_create(&thread, NULL, thread_body, "Child\n");
+    if (code != THREAD_CREATION_SUCCESS) {
+        printError(code, argv);
+        exit(EXIT_ERROR);
     }
-    code2=pthread_join(thread, NULL);
-    if (code2!=NOTERROR) {
-        char buf[256];
-        strerror_r(code, buf, sizeof buf);
-        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
-        exit(1);
+	
+    code = pthread_join(thread, NULL);
+    if (code != THREAD_JOIN_SUCCESS) {
+        printError(code, argv);
+        exit(EXIT_ERROR);
     }
 
-    int i = 0;
-    for (i = 0; i < 10; i++) {
-    	printf("Parent\n");
-    }
+    printLines("Parent\n");
 
     pthread_exit(NULL);
-    return 0;
 }
