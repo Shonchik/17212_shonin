9ed08476dbf96bce95b2e31d85027e9924439ce1
Shonchik <31989755+Shonchik@users.noreply.github.com>
2021-02-15 18:33:49 +0700

    add comment

--- Heads x Improve/Test_Shonin_Dmitry.py
+++ Heads x Improve/Test_Shonin_Dmitry.py
@@ -1,24 +1,27 @@
 import random
 
-MAXNUMBER = 99
-MINNUMBER = 10
+MAXNUMBER = 99 #Максимальная граница чисел
+MINNUMBER = 10 #Минимальная граница чисел
 
 def func(n):
     result = []
-    listLength = random.sample(range(1,n + 5),n)
+    listLength = random.sample(range(1,n + 5),n) #Генерирую список случайных неповторяющихся длин списков размера n
     for tmpLength in listLength:
+        #Создаю и заполняю n списков случайными числами
         result.append([random.randint(MINNUMBER,MAXNUMBER) for j in range(tmpLength)])
 
     for i, tmpList in enumerate(result):
         if i % 2 == 0:
-            tmpList.sort(reverse = False)
+            tmpList.sort(reverse = False) #Списки с четным номером сортирую по возрастанию
         else:
-            tmpList.sort(reverse = True)
+            tmpList.sort(reverse = True) #Списки с нечетным номером сортирую по убыванию
+
     
     return result
 
     
 n = int(input())
 result = func(n)
-for i in result:
+
+for i in result: #Вывожу списки, каждый с новой строки
     print(i)
